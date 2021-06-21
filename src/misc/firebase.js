2607ac8b5622ad610aa746c71bfb46478799a0e9
prm1999 <mourya.18bcs1117@abes.ac.in>
2021-06-21 23:50:38 +0530

    firebase initalization

---
+++ src/misc/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+
+
+const config=        {
+          apiKey: "AIzaSyAYjY_NYfxet1cRKkpRMlUv4zvceEjtvYA",
+          authDomain: "chat-web-app-bd4cb.firebaseapp.com",
+          projectId: "chat-web-app-bd4cb",
+          storageBucket: "chat-web-app-bd4cb.appspot.com",
+          messagingSenderId: "607068527553",
+          appId: "1:607068527553:web:809c8bb9b1b9152268ad40",
+          measurementId: "G-BM3JLRH001"
+        };
+
+ const app=firebase.initializeApp(config);
+
+ app.
