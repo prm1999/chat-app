f6641d31581c0c50494d982c839eeb7ba769c3e2
prm1999 <mourya.18bcs1117@abes.ac.in>
2021-06-22 16:09:17 +0530

    user auth firebase

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,16 +1,19 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
+const config = {
+  apiKey: 'AIzaSyAYjY_NYfxet1cRKkpRMlUv4zvceEjtvYA',
+  authDomain: 'chat-web-app-bd4cb.firebaseapp.com',
+  databaseURL: ' https://chat-web-app-bd4cb-default-rtdb.firebaseio.com/',
+  projectId: 'chat-web-app-bd4cb',
+  storageBucket: 'chat-web-app-bd4cb.appspot.com',
+  messagingSenderId: '607068527553',
+  appId: '1:607068527553:web:809c8bb9b1b9152268ad40',
+  measurementId: 'G-BM3JLRH001',
+};
 
-const config=        {
-          apiKey: "AIzaSyAYjY_NYfxet1cRKkpRMlUv4zvceEjtvYA",
-          authDomain: "chat-web-app-bd4cb.firebaseapp.com",
-          projectId: "chat-web-app-bd4cb",
-          storageBucket: "chat-web-app-bd4cb.appspot.com",
-          messagingSenderId: "607068527553",
-          appId: "1:607068527553:web:809c8bb9b1b9152268ad40",
-          measurementId: "G-BM3JLRH001",
-          databaseURL:" https://chat-web-app-bd4cb-default-rtdb.firebaseio.com/"
-        };
-
- const app=firebase.initializeApp(config);
-
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
+console.log(database)
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,82 @@
 import React from 'react'
+import { Container, Row ,Panel,Col,Icon,Button,Grid, Alert} from 'rsuite'
+import firebase from 'firebase/app';
+
+import { auth, database } from '../misc/firebase';
+
+
+
+
 
 const SignIn = () => {
+
+const signInWithProvider= async provider=>{
+
+try {
+  const {additionalUserInfo,user}= await  auth.signInWithPopup(provider);
+  
+  if(additionalUserInfo.isNewUser){
+   await database.ref(`/profiles/${user.uid}`).set({
+      name:user.displayName,
+      createdAt:firebase.database.ServerValue.TIMESTAMP
+    })
+
+  }
+  Alert.success('SignIn',4000)
+  
+} catch (err) {
+  Alert.info(err.message,4000)
+
+  
+}
+
+
+ 
+};
+
+
+
+  const onFacebookSignIn=()=>{
+
+    signInWithProvider( new firebase.auth.FacebookAuthProvider())
+  };
+
+
+  const onGoogleSignIn=()=>{
+    signInWithProvider(new firebase.auth.GoogleAuthProvider())
+  };
+
+
+
   return (
-    <div>
+    <Container>
+      <Grid className="mt-page">
+      <Row>
+        <Col xs={24} md={12} mdOffset={6}>
+          <Panel>
+            <div className="text-center ">
+              <h2 className="text-green">
+                Welcome To Chat APP
+              </h2>
+              <p>Progressive web PlateForm</p>
+
+
+            </div>
+            <div className="mt-3">
+            <Button block color="blue" onClick={onFacebookSignIn}>
+            <Icon icon="facebook"/> Continue with facebook
+            </Button>
+
+            <Button block color="green" onClick={onGoogleSignIn}>
+            <Icon icon="google"/> Continue with google
+            </Button>
+            </div>
+          </Panel>
+        </Col>
+      </Row>
+      </Grid>
       SignIn
-    </div>
+    </Container>
   )
 }
 
