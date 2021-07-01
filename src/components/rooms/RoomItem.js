554e0c106b08602974b7e8831e238f425de0eccf
prm1999 <mourya.18bcs1117@abes.ac.in>
2021-07-01 22:44:00 +0530

    deploy

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,50 +1,45 @@
-import React from 'react'
-import TimeAgo from 'timeago-react'
-import ProfileAvtar  from "../ProfileAvtar"
+import React from 'react';
+import TimeAgo from 'timeago-react';
+import ProfileAvtar from '../ProfileAvtar';
 
-const RoomItem = ({room}) => {
-
-const {createdAt,name ,lastMessage,file}=room;
+const RoomItem = ({ room }) => {
+  const { createdAt, name, lastMessage, file } = room;
 
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
         <h3 className="text-disappear"> {name}</h3>
-        <TimeAgo 
-        datetime={lastMessage ? new Date(lastMessage.createdAt):new Date(createdAt)}
- 
-        
-        className="font-normal text-black-45"
+        <TimeAgo
+          datetime={
+            lastMessage ? new Date(lastMessage.createdAt) : new Date(createdAt)
+          }
+          className="font-normal text-black-45"
         />
-        
       </div>
-
 
       <div className="d-flex align-items-center text-black-70">
-      {
-        lastMessage ?
-        <>
+        {lastMessage ? (
+          <>
+            <div className="d-flex align-items-center">
+              <ProfileAvtar
+                src={lastMessage.author.avatar}
+                name={lastMessage.author.name}
+                size="sm"
+              />
+            </div>
 
-        <div className="d-flex align-items-center">
-          <ProfileAvtar src={lastMessage.author.avatar} name={lastMessage.author.name} size="sm"/>
-        </div>
-
-        <div className="text-disappear ml-2">
-          <div className="italic">{lastMessage.author.name}</div>
-          <span> {lastMessage.text||lastMessage.file.name}</span>
-          {/* || lastMessage.file.name */}
-        </div>
-
-
-        </>:
-          <span > No message Yet...</span>
-
-      }
-
-     
+            <div className="text-disappear ml-2">
+              <div className="italic">{lastMessage.author.name}</div>
+              <span> {lastMessage.text}</span>
+              {/* || lastMessage.file.name */}
+            </div>
+          </>
+        ) : (
+          <span> No message Yet...</span>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RoomItem
+export default RoomItem;
